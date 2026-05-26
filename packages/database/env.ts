a04194b70c5f9ad68d0cfda1e21d2ec23fd9d88b
Ankit Kumar Singh <ankitkrsingh0207@gmail.com>
2026-05-27 04:54:48 +0530

    changes

--- packages/database/env.ts
+++ packages/database/env.ts
@@ -1,13 +1,27 @@
+import dotenv from "dotenv";
 import { z } from "zod";
+
+dotenv.config({
+  path: "../../.env",
+});
 
 const envSchema = z.object({
   DATABASE_URL: z.string().describe("DB URL"),
 });
 
 function createEnv(env: NodeJS.ProcessEnv) {
-  const safeParseResult = envSchema.safeParse(env);
-  if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+
+  const safeParseResult =
+    envSchema.safeParse(env);
+
+  if (!safeParseResult.success) {
+    throw new Error(
+      safeParseResult.error.message
+    );
+  }
+
   return safeParseResult.data;
 }
 
-export const env = createEnv(process.env);
+export const env =
+createEnv(process.env);
