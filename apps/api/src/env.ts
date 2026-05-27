38840843b013577602ebe6eaeb6c7a0e3dc4cf2a
Ankit Kumar Singh <ankitkrsingh0207@gmail.com>
2026-05-27 08:03:35 +0530

    fix NODE_ENV production

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -2,14 +2,28 @@ import { z } from "zod";
 
 const envSchema = z.object({
   PORT: z.string().optional(),
-  NODE_ENV: z.enum(["development", "prod"]).default("development"),
-  BASE_URL: z.string().default("http://localhost:8000"),
+
+  NODE_ENV: z
+    .enum(["development", "production"])
+    .default("development"),
+
+  BASE_URL: z
+    .string()
+    .default("http://localhost:8000"),
 });
 
 function createEnv(env: NodeJS.ProcessEnv) {
-  const safeParseResult = envSchema.safeParse(env);
-  if (!safeParseResult.success) throw new Error(safeParseResult.error.message);
+  const safeParseResult =
+    envSchema.safeParse(env);
+
+  if (!safeParseResult.success) {
+    throw new Error(
+      safeParseResult.error.message
+    );
+  }
+
   return safeParseResult.data;
 }
 
-export const env = createEnv(process.env);
+export const env =
+  createEnv(process.env);
